ea139bd1bab4df34a6089c4561a040af78cf630d
sjoenH <henryskorpe@gmail.com>
2015-03-17 10:05:01 +0100

    Started sketching out program

--- BrannbotSam/SamsLittleHelpers/SamsLittleHelpers.ino
+++ BrannbotSam/SamsLittleHelpers/SamsLittleHelpers.ino
@@ -1,7 +1,14 @@
-void setup(){
-  //Setup Things...
+#include "PLab_IRremote.h"
+IRsend irsend;
+
+void setup()
+{
+  /*Serial.begin(9600); */ //Dont think we need this...
 }
 
-void loop(){
-  //Main program
+void loop()
+{
+  if (Serial.read() != -1) { //TODO: if Robot detects Fire (or buttonpress)
+    irsend.sendNEC(IR_UP, 32); //Send my position aka. destination
+  }
 }
