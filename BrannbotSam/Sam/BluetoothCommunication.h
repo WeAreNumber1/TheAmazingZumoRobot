084824ef5722f14252b07c053f780edd94134e83
tobinus <thorben@sjostrom.no>
2015-04-12 20:38:06 +0200

    Made header-file

---
+++ BrannbotSam/Sam/BluetoothCommunication.h
@@ -0,0 +1,13 @@
+#ifndef BT_COMMUNICATION_SAM
+#define BT_COMMUNICATION_SAM
+
+#include <SoftwareSerial.h>
+#include <PLabBTSerial.h>
+
+void BT_sendHasReturned();
+int BT_getDestination();
+boolean BT_shallReturn();
+boolean BT_hasNewDestination();
+void BT_update();
+
+#endif
